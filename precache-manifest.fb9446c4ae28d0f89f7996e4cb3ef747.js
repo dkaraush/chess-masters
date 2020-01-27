b0a9c58a69e4bf94a093d5344e498671434d8943
dkaraush <dkaraush@gmail.com>
2020-01-27 13:16:39 +0200

    Updates

---
+++ precache-manifest.fb9446c4ae28d0f89f7996e4cb3ef747.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250689f13ce5724186d5fb5b4b2b08ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5205dde7a6749b400edf",
+    "url": "/static/css/main.f0a9c2eb.chunk.css"
+  },
+  {
+    "revision": "1ade8ff7253d7c1fa15e",
+    "url": "/static/js/2.2cf3f9b3.chunk.js"
+  },
+  {
+    "revision": "cf80d234201654446deddfe5c22c990b",
+    "url": "/static/js/2.2cf3f9b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5205dde7a6749b400edf",
+    "url": "/static/js/main.820e6392.chunk.js"
+  },
+  {
+    "revision": "fb73a5fd6b0ecbdc79e8",
+    "url": "/static/js/runtime-main.4ef7cf1e.js"
+  },
+  {
+    "revision": "25bfae6672e8ac9c44f455fd8e9574d8",
+    "url": "/static/media/logo.25bfae66.svg"
+  }
+]);
